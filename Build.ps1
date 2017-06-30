141b5b72fbbcc99dc5851d9983afc09548a1a2dc
gobm <gobm@PCSOR274>
2017-06-30 17:30:42 -0300

    adjusting publish after build

--- Build.ps1
+++ Build.ps1
@@ -1,6 +1,11 @@
 $msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
-$path = (Resolve-Path .\).Path + "\DataBaseConnection.sln /p:Configuration=""Release"""
+$path = (Resolve-Path .\).Path + "\DataBaseConnection.sln /p:Configuration=""Release"" /p:DeployOnBuild=true /p:PublishProfile=""FolderProfile"" "
 $build = $msbuild + " " + $path
-Write-host $build
-#$msbuild DataBaseConnection.sln /t:"DataBaseConnection" /p:Configuration="Release" 
+
 Invoke-Expression $build
+
+If ($LastExitCode -eq 0) {
+Write-host "OK"
+}
+
+
