89affe7b43f2d18b78260de4a880af84bb9b9e4c
gobm <gobm@PCSOR274>
2017-06-30 17:49:52 -0300

    adjusting publish settings

--- Build.ps1
+++ Build.ps1
@@ -2,10 +2,16 @@ $msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
 $path = (Resolve-Path .\).Path + "\DataBaseConnection.sln /p:Configuration=""Release"" /p:DeployOnBuild=true /p:PublishProfile=""FolderProfile"" "
 $build = $msbuild + " " + $path
 
-Invoke-Expression $build
+Invoke-Expression $build 
 
 If ($LastExitCode -eq 0) {
-Write-host "OK"
+	Remove-Item C:\Inetpub\wwwroot\* -recurse
+
+	#Get-ChildItem -Path C:\Inetpub\wwwroot -Include *.* -File -Recurse | foreach { $_.Delete()}
+	
+	$releaseFolder = (Resolve-Path .\).Path + "\DataBaseConnection\bin\Release\PublishOutput\*"
+	
+	Move-Item $releaseFolder "C:\Inetpub\wwwroot"
 }
 
 
--- DataBaseConnection/bin/Release/PublishOutput/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DataBaseConnection.MvcApplication" Language="C#" %>
