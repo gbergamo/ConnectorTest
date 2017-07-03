81098f03a5f4d9c27b38c78319c7e780af4de0b2
gobm <gobm@PCSOR274>
2017-07-03 13:07:38 -0300

    complete path

--- Build.ps1
+++ Build.ps1
@@ -1,18 +1,17 @@
 #$msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
-$msbuild = (Resolve-Path .\).Path + "\Files\msbuild.exe"
-$path = (Resolve-Path .\).Path + "\DataBaseConnection.sln /p:Configuration=""Release"" /p:DeployOnBuild=true /p:PublishProfile=""FolderProfile"" "
+#$msbuild = (Resolve-Path .\).Path + "\Files\msbuild.exe"
+
+$msbuild = 'C:\ConnectorTest\Files\msbuild.exe'
+$path = 'C:\ConnectorTest\DataBaseConnection.sln /p:Configuration="Release" /p:DeployOnBuild=true /p:PublishProfile="FolderProfile" '
 $build = $msbuild + " " + $path
 
+Write-host (Resolve-Path .\).Path
 Invoke-Expression $build 
 
 If ($LastExitCode -eq 0) {
-	Remove-Item C:\Inetpub\wwwroot\* -recurse
-
-	#Get-ChildItem -Path C:\Inetpub\wwwroot -Include *.* -File -Recurse | foreach { $_.Delete()}
-	
-	$releaseFolder = (Resolve-Path .\).Path + "\DataBaseConnection\bin\Release\PublishOutput\*"
-	
-	Move-Item $releaseFolder "C:\Inetpub\wwwroot"
+	Remove-Item C:\Inetpub\wwwroot\* -recurse	
+	$releaseFolder = 'C:\ConnectorTest\DataBaseConnection\bin\Release\PublishOutput\*'
+	Move-Item $releaseFolder 'C:\Inetpub\wwwroot'
 }
 
 
