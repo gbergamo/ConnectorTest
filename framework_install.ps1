14131eb25f629a83de36448a85121915eceae9b3
gobm <gobm@PCSOR274>
2017-07-03 10:13:13 -0300

    adjusting framework powershell installer

--- framework_install.ps1
+++ framework_install.ps1
@@ -1,1 +1,2 @@
-Start-Process -FilePath D:\path\to\dotnetfx45_full_x86_x64.exe -ArgumentList "/q /norestart" -Wait -Verb RunAs
+$path = "" + $(get-location) + "\files\NDP452-KB2901907-x86-x64-AllOS-ENU.exe"
+Start-Process -FilePath $path -ArgumentList "/q /norestart" -Wait -Verb RunAs
