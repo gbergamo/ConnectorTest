37c56b4dee2ba4b5b00f818866d3b3583169c290
gobm <gobm@PCSOR274>
2017-07-03 10:47:42 -0300

    get msbuild from proj folder

--- Build.ps1
+++ Build.ps1
@@ -1,4 +1,5 @@
-$msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
+#$msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
+$msbuild = (Resolve-Path .\).Path + "\Files\msbuild.exe"
 $path = (Resolve-Path .\).Path + "\DataBaseConnection.sln /p:Configuration=""Release"" /p:DeployOnBuild=true /p:PublishProfile=""FolderProfile"" "
 $build = $msbuild + " " + $path
 
